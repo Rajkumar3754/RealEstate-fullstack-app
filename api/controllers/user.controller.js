5175b92e0c03241cc9e53f9b81e7818f259a3f88
Rajkumar <u.rajkumar2001@gmail.com>
2024-07-30 12:33:14 +0530

    created signup api routes

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,7 @@
+
+// controller for test api  routes
+
+export const test = (req,res)=>{
+    res.send("Express is running")
+
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from '../api/routes/user.route.js'
+import authRouter from '../api/routes/auth.route.js'
 
 dotenv.config()
 
@@ -15,6 +17,8 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("Connected to mongodb"))
 .catch(()=>console.log("failed to connected"))
 
+app.use(express.json())
+
 
 
 
@@ -23,3 +27,9 @@ app.listen(port,()=>{
 
     console.log(`server running on ${port}`)
 })
+
+
+
+app.use('/api/user',userRouter)
+
+app.use('/api/auth',authRouter)
