655511826962d9eb3546450b658fb3638b778c7b
Rajkumar <u.rajkumar2001@gmail.com>
2024-08-07 10:20:16 +0530

    created get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utlis/error.js";
 
@@ -77,5 +78,37 @@ export const deleteUser =  async(req,res,next)=>{
             
         }
     
+
+}
+
+
+export const getUserListings = async (req,res,next)=>{
+
+    if(req.user.id === req.params.id){
+
+
+        try {
+
+            const listings = await Listing.find({userRef:req.params.id});
+
+            res.status(200).json(listings);
+            
+        } catch (error) {
+
+            next(error)
+            
+        }
+
+
+
+
+    }else{
+
+        return next(errorHandler(401,"You can only view your own listings"));
+
+
+    }
+
+
 
 }
