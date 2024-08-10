e85514665e1a5bcef70b13c41c9efffe620094c1
Rajkumar <u.rajkumar2001@gmail.com>
2024-08-10 12:06:32 +0530

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-center text-slate-800 '>
+        About Eclipse Estate
+      </h1>
+
+      <p className='mb-4 text-slate-700'>Welcome to Eclipse Estate, where luxury and nature converge to create an unparalleled living experience. Nestled in a serene setting, Eclipse Estate offers a harmonious blend of modern design and natural beauty. Each property is thoughtfully crafted with premium materials and innovative features, ensuring a lifestyle of comfort and sophistication. Whether you’re enjoying a quiet moment on your private terrace or hosting an elegant soirée, Eclipse Estate provides the perfect backdrop for every occasion.</p>
+      
+      <p className='mb-4 text-slate-700' >At Eclipse Estate, sustainability and elegance go hand in hand. Our commitment to environmental responsibility is reflected in every aspect of the estate, from energy-efficient systems to sustainable landscaping. The estate features advanced green technologies, including solar panels and water recycling systems, designed to minimize your ecological footprint while maximizing your quality of life. We believe that luxury should not come at the expense of the environment, and Eclipse Estate is a testament to this philosophy.</p>
+      
+      <p className='mb-4 text-slate-700'  >Explore a community that celebrates both exclusivity and connectivity. Eclipse Estate is more than just a collection of homes; it’s a vibrant neighborhood where residents can connect and thrive. Enjoy a range of world-class amenities, including a state-of-the-art fitness center, tranquil walking trails, and a community clubhouse for social gatherings. With its prime location, Eclipse Estate offers convenient access to top-tier schools, shopping, and dining, making it an ideal place for families and professionals alike.</p>
     </div>
   )
 }
